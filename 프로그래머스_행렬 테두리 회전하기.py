159391aa98b1974a4f33a3343a2c1af72ebc4625
jongbin <runnz121@gmail.com>
2021-12-22 20:25:20 +0900

    행렬 테두리 회전하기

--- 프로그래머스_행렬 테두리 회전하기.py
+++ 프로그래머스_행렬 테두리 회전하기.py
@@ -1,86 +1,38 @@
-import copy
-from collections import deque
-
 def solution(rows, columns, queries):
     answer = []
+    table = []
+    for r in range(rows):
+        table.append([a for a in range(r * columns + 1, (r + 1) * columns + 1)])
 
-    board = [[]*columns for _ in range(rows)]
-    each = []
-    idx = []
-    check = deque()
+    for query in queries:
+        query = [x - 1 for x in query]  # 0부터 시작하는 인덱스에 맞춰 1씩 빼줌
+        tmp = table[query[0]][query[1]]  # 왼쪽 위 값 저장
+        small = tmp
 
-    for i in range(1, rows * columns+1):
-        each.append(i)
-    for j in range(0, len(each) - 1, columns):
-        idx.append(j)
+        # left
+        for i in range(query[0] + 1, query[2] + 1):
+            table[i - 1][query[1]] = table[i][query[1]]
+            small = min(small, table[i][query[1]])
+        # bottom
+        for i in range(query[1] + 1, query[3] + 1):
+            table[query[2]][i - 1] = table[query[2]][i]
+            small = min(small, table[query[2]][i])
+        # right
+        for i in range(query[2] - 1, query[0] - 1, -1):
+            table[i + 1][query[3]] = table[i][query[3]]
+            small = min(small, table[i][query[3]])
+        # top
+        for i in range(query[3] - 1, query[1] - 1, -1):
+            table[query[0]][i + 1] = table[query[0]][i]
+            small = min(small, table[query[0]][i])
+        table[query[0]][query[1] + 1] = tmp
 
-    for i in range(rows):
-        board[i] = each[idx[i]:idx[i] + columns]
+        answer.append(small)
 
-
-
-    # print(board)
-
-    for i in range(len(queries)):
-        x1 = queries[i][0] -1
-        y1 = queries[i][1] -1
-        x2 = queries[i][2] -1
-        y2 = queries[i][3] -1
-        # print(x1, y1, x2, y2)
-        #row가 같을때 1
-        for a in range(x1, x1 + abs(x1-x2)):
-            check.append(board[y1][a])
-            print('1',check)
-        #column이 같을때 1
-        for b in range(y1, y1 + abs(y1-y2)+1):
-            check.append(board[b][x2-1])
-            print('2', check)
-        #row가 같을때 2
-        for c in range(x2, abs(x1-x2)-1, -1):
-            check.append(board[y2+1][c])
-            print('3', check)
-        #colum 같을때 2
-        for d in range(y1 + abs(y1-y2), y1, -1):
-            check.append(board[d][x1])
-            print('4', check)
-        check.appendleft(check.pop())
-
-
-        #check 큐에서 꺼내서 재배열
-        answer.append(min(check))
-        for a in range(x1, x1 + abs(x1 - x2)):
-            board[y1][a] = check.popleft()
-
-        for b in range(y1+1, y1 + abs(y1-y2)+1):
-            board[b][x2-1] = check.popleft()
-
-        # print('1', check)
-        for d in range(y1 + abs(y1-y2)+1, y1, -1):
-            board[y2+1][d-1] = check.popleft()
-        # print('1', board)
-
-        # print('2', check)
-        for d in range(y1 + abs(y1-y2), y1, -1):
-            board[d][x1] = check.popleft()
-        # print('2',board)
-    print(answer)
+    # print(answer)
     return answer
-rows = 6
-columns = 6
-queries = [[2, 2, 5, 4], [3, 3, 6, 6], [5, 1, 6, 3]]
-sample = [[2,2,5,4]]
-solution(rows, columns, sample)
 
-#     check.append(board[y1][x1:x2])
-#     check.append(board[y2+1][x1:x2])
-# arr_a= []
-# for a in range(y1, y1+abs(y1-y2)+1):
-#     arr_a.append(board[a][x2-1])
-# check.append(arr_a)
-#
-# arr_b = []
-# for b in range(y2+1, y2 - abs(y1-y2),-1):
-#     arr_b.append(board[b][x1])
-# check.append(arr_b)
-#
-# print(check)
+rows = 6
+columns= 6
+queries = [[2,2,5,4],[3,3,6,6],[5,1,6,3]]
+solution(rows, columns, queries)
