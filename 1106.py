e66a9c00a2487a685cb05593037d51aeb48981ac
runnz121 <runnz121@gmail.com>
2022-02-27 02:52:58 +0900

    daily

--- 1106.py
+++ 1106.py
@@ -20,29 +20,32 @@
 #
 # # print(data_sort)
 #
-# for cost, cus in data_sort:
+# for cost, cus in data:
+#     print(data)
 #     for i in range(cus, c+100): # 최소값 지정해주기 위해서
 #         min_cost[i] = min(min_cost[i-cus] + cost, min_cost[i])
 #
 # print(min(min_cost[c:]))
-#
+
 
 n, m = map(int, input().split())
 INF = int(1e9)
-sam = 20
 
-dp = [[0] + [sam] * (n + 1) for _ in range(m + 1)]
+#dp = [[0] + [INF] * (n + 1) for _ in range(m + 1)]
 
+dp = [INF] * 101
+dp[0] = 0
 
-#print(dp)
-
-for i in range(1, m+1):
+for _ in range(1, m + 1):
     cost, people = map(int, input().split())
 
-    for j in range(1, n+1):
-        if j >= people:
-            dp[i][j] = min(dp[i-1][j], dp[i-1][j-people] + cost)
-        else:
-            dp[i][j] = dp[i-1][j]
+    for j in range(cost, n + 1):
+        print(cost, people)
+        #   if j >= people:
+        dp[j] = min(dp[j-people] + cost, dp[j])
 
+        #dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - people] + cost)
+    # else:
+    #     dp[i][j] = dp[i-1][j]
 print(dp)
+print(dp[n])
--- sample.py
+++ sample.py
@@ -1,2 +1,26 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
 
-print(5//2)
+v, e = map(int, input().split())
+
+edge = []
+
+distance = [INF] * (v + 1)
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    edge.append((a,b,c))
+
+print(edge)
+
+def bell(start):
+    distance[start] = 0
+
+    for i in range(v): # 노드 갯수
+        for j in range(e): # 간선갯수
+            currnet = edge[j][0]
+            next = edge[j][1]
+            cost = edge[j][2]
+
+            if distance[currnet] != INF and distance[next] > distance[currnet] + cost:
