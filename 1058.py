183285a7b96e88f7ed174532326be82e18cbfbd0
jongbin <runnz121@joongna.co.kr>
2022-04-29 21:34:56 +0900

    daily

--- 1058.py
+++ 1058.py
@@ -11,12 +11,11 @@ check = [[0] * n for _ in range(n)]
 for k in range(n):
     for i in range(n):
         for j in range(n):
+            print(k,i,j)
             if i == j:
                 continue
-
             if graph[i][j] == 'Y' or (graph[i][k] == 'Y' and graph[k][j] == 'Y'):
                 check[i][j] = 1 # 서로 아는 친구가 있으면 1로 초기화
-
 for x in check:
     ans = max(ans, sum(x))
 print(ans)
