e09e4287d66fc02539c1d0777aa8d8742ac5702f
박종빈 <runnz121@joonggonara.co.kr>
2022-06-06 23:56:44 +0900

    daily

---
+++ 11660.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+# https://castlerain.tistory.com/100
+n, m = map(int, input().split())
+
+graph = []
+
+for i in range(n):
+    x = list(map(int, input().split()))
+    graph.append(x)
+
+sum_data = [[0] * (n+1) for i in range(n+1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        sum_data[i][j] = sum_data[i][j-1] + sum_data[i-1][j] - sum_data[i-1][j-1] + graph[i-1][j-1]
+
+
+for k in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+
+    print(sum_data[x2][y2] - sum_data[x1-1][y2] - sum_data[x2][y1-1] + sum_data[x1-1][y1-1])
