32056a90c3ead8e83f097550f2c7c940bd3a05bc
박종빈 <runnz121@joonggonara.co.kr>
2022-06-13 22:50:35 +0900

    daily

---
+++ 3711.py
@@ -0,0 +1,16 @@
+while True:
+    n = int(input())
+
+    mins = int(1e9)
+
+
+    arr = []
+    for i in range(n):
+        x = int(input())
+        arr.append(x)
+
+    for k in range(max(arr)):
+        for p in arr:
+            if 
+
+    print(mins)
