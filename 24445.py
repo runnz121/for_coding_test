e2c5ba55dafaf71cb319475f0172fbdd09d3eec1
박종빈 <runnz121@joonggonara.co.kr>
2022-06-20 22:40:34 +0900

    daily

---
+++ 24445.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n, m, r = map(int, input().split())
+
+check = [False] * (n + 1)
+
+graph = [[] for _ in range(n + 1)]
+
+for i in range(n):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+
+for i in graph:
+    i.sort(reverse=True)
+
+ans = [0] * (n + 1)
+
+def bfs(start):
+    cnt = 0
+    queue = deque([start])
+    check[start] = True
+
+    while queue:
+        v = queue.popleft()
+        cnt += 1
+        ans[v] = cnt
+
+        for k in graph[v]:
+            if not check[k]:
+                check[k] = True
+                queue.append(k)
+
+bfs(r)
+
+for i in range(1, len(ans)):
+    print(ans[i])
+
