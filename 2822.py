c6239303e5c30b692d77043c31e65e8c3b82124b
박종빈 <runnz121@gmail.com>
2022-08-04 23:11:56 +0900

    daily

--- 2822.py
+++ 2822.py
@@ -1,9 +1,8 @@
 db = dict()
 
-for i in range(1, 9):
+for i in range(8):
     x = int(input())
-    if x not in db:
-        db[i] = x
+    db[i] = x
 
 res = sorted(db.items(), key = lambda x : x[1], reverse=True)
 
@@ -11,7 +10,7 @@ ans = []
 sums = 0
 for k in range(len(res)-3):
     sums += res[k][1]
-    ans.append(res[k][0])
+    ans.append(res[k][0] + 1)
 ans.sort()
 print(sums)
 print(*ans)
