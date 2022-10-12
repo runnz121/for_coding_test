7feec034ab1088a3aff3a13e8c0db4772a7a72c3
박종빈 <runnz121@gmail.com>
2022-10-13 00:11:26 +0900

    daily

--- 2605.py
+++ 2605.py
@@ -1,9 +1,10 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-line = [0] * n
+ans = []
 
-line[n-1] = 1
+for i in range(n):
+    ans.insert(i-arr[i], i + 1)
 
-for i in range(1, n):
-    if line[i] != 0:
+for k in ans:
+    print(k, end=' ')
