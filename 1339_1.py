594f2997540c98d21b4ac46a5264e9e480bf7e88
runnz121 <runnz121@gmail.com>
2022-10-25 20:33:06 +0900

    daily

--- 1339_1.py
+++ 1339_1.py
@@ -1,48 +1,33 @@
-import copy
 import sys
 
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
 
-n = int(input())
-arr = []
-db = set()
+alpha = [] # 단어를 저장할 리스트
+alpha_dict = {} # 단어 내의 알파벳별로 수를 저장할 딕셔너리
+numList = [] # 수를 저장할 리스트
 
-for i in range(n):
-    x = str(input().rstrip())
-    arr.append(x)
-    x_list = list(x)
+for i in range(n): # 단어를 입력받음
+    alpha.append(sys.stdin.readline().rstrip())
 
-    for k in x_list:
-        db.add(k)
-db = list(db)
-db.sort()
+for i in range(n): # 모든 단어에 대해서
+    for j in range(len(alpha[i])): # 단어의 길이만큼 실행
 
+        if alpha[i][j] in alpha_dict: # 단어의 알파벳이 이미 dict에 있으면
+            alpha_dict[alpha[i][j]] += 10 ** (len(alpha[i])-j-1) # 자리에 맞게 추가 ( 1의 자리면 1 )
+        else:   # 자리에 없으면 새로 추가 ( 10의 자리면 10 )
+            alpha_dict[alpha[i][j]] = 10 ** (len(alpha[i])-j-1)
 
-number = [i for i in range(9, 9-len(db), -1)]
+for val in alpha_dict.values(): # dict에 저장된 수들을 모두 리스트에 추가
+    numList.append(val)
 
-check = [False] * (len(db))
-numbers_arr = []
-tmp = []
-def back(depth):
+print(numList)
 
-    global numbers_arr
-    global tmp
+numList.sort(reverse=True) # 수들을 내림차순 정렬
 
-    tmp1 = copy.deepcopy(tmp)
-
-    if depth == len(db):
-        numbers_arr.append(tmp1)
-        return
-
-    for i in range(len(db)):
-        if check[i]:
-            continue
-
-        tmp.append(number[i])
-        check[i] = True
-        back(depth + 1)
-        check[i] = False
-        tmp.pop()
-back(0)
-
-for idx in numbers_arr:
+sum = 0
+pows = 9
+for i in numList: # 첫 번째 부터 가장 큰 부분을 차지하므로 9를 곱해준다
+    sum += pows * i
+    pows -= 1
+# 내려갈수록 그 알파벳이 차지하는 비중이 적으므로 -1
+print(sum)
