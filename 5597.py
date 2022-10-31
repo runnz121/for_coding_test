fbc0609652dd0184f16adeaf7e1a5ca8d006a9cc
runnz121 <runnz121@gmail.com>
2022-10-31 20:39:23 +0900

    daily

--- 5597.py
+++ 5597.py
@@ -1,7 +1,17 @@
 arr = []
-while True:
-    x = int(input())
+for i in range(30):
+    x = str(input())
     arr.append(x)
 
 arr.sort()
 
+check = []
+
+for i in range(1, 31):
+    if str(i) not in arr:
+        check.append(i)
+
+check.sort()
+
+for i in check:
+    print(i)
