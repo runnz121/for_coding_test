bfb0efade3164baaa467a9123d6d39240b0eff56
runnz121 <runnz121@gmail.com>
2022-11-08 23:08:17 +0900

    Daily

--- 1238.py
+++ 1238.py
@@ -7,15 +7,9 @@ n, m, x = map(int, input().split())
 # node
 node = [[] for _ in range(n + 1)]
 
-# check
-visited = [False] * (n + 1)
-
-# minDist
-minDist = [0] * (n + 1)
-
-for i in range(m):
+for _ in range(m):
     x, y, z = map(int, input().split())
-    node[x].append([y,z])
+    node[x].append((y, z))
 
 def dijkstra(start):
     q = []
@@ -29,14 +23,14 @@ def dijkstra(start):
         dist, now = heapq.heappop(q)
 
         if distance[now] < dist:
-            minDist[now] += dist
             continue
 
-        for i in node[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+        for idx, node_cost in node[now]:
+            cost = node_cost + dist
+
+            if distance[idx] > cost:
+                distance[idx] = cost
+                heapq.heappush(q, (cost, idx))
 
     return distance
 
@@ -46,7 +40,6 @@ for i in range(1, n + 1):
     go = dijkstra(i)
     # 도착지점에서 시작
     back = dijkstra(x)
-
     # 시작지점에서 출발해서 도착한 지점 + 도착지점일때 시작한 지점위치
     res = max(res, go[x] + back[i])
 
