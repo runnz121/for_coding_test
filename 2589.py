c341aed896c933b1d58348702587c0f57f6dc0a3
runnz121 <runnz121@gmail.com>
2022-12-25 00:02:19 +0900

    daily

--- 2589.py
+++ 2589.py
@@ -1,5 +1,7 @@
+import sys
 from collections import deque
 
+input = sys.stdin.readline
 n, m = map(int, input().split())
 
 arr = []
@@ -7,43 +9,33 @@ for i in range(n):
     x = list(str(input()))
     arr.append(x)
 
-start = []
-save = []
-
 dx = [-1, 1, 0, 0]
-dy = [0,0,-1,1]
+dy = [0,0,1,-1]
 
-def bfs(x, y):
-    global start
-    global save
-
-    check = [[False for _ in range(m)] for _ in range(n)]
-    check[x][y] = True
+def bfs(cost, x, y):
+    check = [[0] * m for _ in range(n)]
+    check[x][y] = 1
     q = deque()
-    start.append([x, y])
-    child = []
-    dist = 0
+    q.append([cost, x, y])
+    longest = 0
     while q:
-        x1, y1 = q.popleft()
-
+        cost, x1, y1 = q.popleft()
+        longest = max(longest, cost)
         for i in range(4):
             nx = x1 + dx[i]
             ny = y1 + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m:
-                if not check[nx][ny]:
-                    if arr[nx][ny] == 'L':
-                        dist += 1
-                        q.append([nx, ny])
-                        child.append([nx, ny, dist])
-                        check[nx][ny] = True
-                        print(nx, ny, dist)
-    save.append(child)
+            if 0 <= nx < n and 0 <= ny < m and check[nx][ny] == 0 and arr[nx][ny] == 'L':
+                q.append([cost + 1, nx, ny])
+                check[nx][ny] = 1
+    return longest
 
+
+max_dist = 0
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 'L':
-            bfs(i, j)
+            dist = bfs(0, i, j)
+            max_dist = max(dist, max_dist)
 
-print(start)
-print(save)
+print(max_dist)
