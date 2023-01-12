a9853d963bb0c337538bb677a3db77602d14adab
runnz121 <runnz121@gmail.com>
2023-01-12 23:14:30 +0900

    Daily

--- 1431.py
+++ 1431.py
@@ -1,3 +1,19 @@
 n = int(input())
 
-lis = input()
+def sums(lists):
+    tot = 0
+    for i in lists:
+        if i.isdigit():
+            tot += int(i)
+    return tot
+
+arr = []
+
+for k in range(n):
+    lis = input()
+    arr.append(lis)
+
+arr.sort(key = lambda x : (len(x), sums(x), x))
+
+for k in arr:
+    print(k)
