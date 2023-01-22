d042c9f11bbc8070e6fa0c6f2421d2edf7d45b1f
runnz121 <runnz121@gmail.com>
2023-01-22 23:23:19 +0900

    daily

--- 16194.py
+++ 16194.py
@@ -1,4 +1,14 @@
-n = int(input())
+N = int(input())
 
-arr = list(map(int, input().split()))
+p = [0] + list(map(int,input().split()))
+dp = [False for _ in range(N+1)]
 
+
+for i in range(1, N+1):
+    for k in range(1, i+1):
+        if dp[i] == False :
+            dp[i] = dp[i-k]+p[k]
+        else :
+            dp[i] = min(dp[i], dp[i-k]+p[k])
+
+print(dp[N])
